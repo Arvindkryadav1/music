847b9b33bb416d575c41ef6cac72a7007d261cea
Arvind Kumar <aarviiyadav19@gmail.com>
2024-04-14 23:29:48 +0530

    music school restart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinar from "@/components/UpcomingWebinar";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <MusicSchoolTestimonials />
+      <UpcomingWebinar />
     </main>
   );
 }
